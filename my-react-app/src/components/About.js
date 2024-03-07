c0253fbf063e00223cbd5d5ee80cf2135d088430
Nate Limono <natelimono@gmail.com>
2024-03-07 14:55:18 -0800

    added about page

---
+++ my-react-app/src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function About() {
+    return(
+        <div className="About-container">
+            <p>HEEELLOOOOOOO</p>
+        </div>
+    )
+}
+export default About;
--- my-react-app/src/components/App.js
+++ my-react-app/src/components/App.js
@@ -1,22 +1,18 @@
-import logo from '../logo.svg';
 import '../App.css';
+import About from "./About.js";
 
+
+
+import {Nav} from 'react-bootstrap'
+import {Navigate} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import {useParams} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <About/>
       </header>
     </div>
   );
